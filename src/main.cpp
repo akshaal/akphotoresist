acd573930907dbf3b1e3e721a6f6775dccdfefe9
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2017-04-27 23:35:02 +0200

    more work

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,8 @@ AKAT_DECLARE(/* cpu_frequency = */              1000000,
              /* dispatcher_idle_code = */       ,
              /* dispatcher_overflow_code = */   )
 
-AKAT_DEFINE_OUT_PIN(error_led, B, 0)
+AKAT_DEFINE_OUTPUT_PIN(error_led, B, 0)
+AKAT_DEFINE_INPUT_PIN_WITH_INTERNAL_PULLUP(key1, D, 2)
 
 __ATTR_NORETURN__
 void main () {
@@ -16,19 +17,10 @@ void main () {
     error_led.set(0);
     error_led.enable_output_mode();
 
+    key1.enable_input_with_internal_pullup();
+
     while(1) {
-        error_led.set(0);
-        akat_delay_ms(200);
-        akat_delay_ms(200);
-        akat_delay_ms(200);
-        akat_delay_ms(200);
-        akat_delay_ms(200);
-        error_led.set(1);
-        akat_delay_ms(200);
-        akat_delay_ms(200);
-        akat_delay_ms(200);
-        akat_delay_ms(200);
-        akat_delay_ms(200);
+        error_led.set(!key1.read());
     }
 
     akat_dispatcher_loop ();
