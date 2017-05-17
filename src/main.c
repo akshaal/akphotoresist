7fdab58757d7c71f0b4fe48a9996b9ee6ad39e1e
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2017-05-17 22:44:17 +0200

    blah

--- src/main.c
+++ src/main.c
@@ -4,7 +4,11 @@
 
 WRITE_CFLAGS$(firmware);
 
+// Declare error led
 X_GPIO_OUTPUT$(error_led, B0);
+
+// Declare variable with timestamp (contains hh, mm, ss, decis)
+X_TIMESTAMP$(timestamp);
 
 X_FULL_BUTTON$(button1, D2) {
     METHOD$(void on_press()) {
@@ -16,6 +20,18 @@ X_FULL_BUTTON$(button1, D2) {
     }
 }
 
+
+X_CLOCK$(c, timestamp);
+
+X_TIMESTAMP_CALLBACKS$(timestamp) {
+    METHOD$(void on_new_second_l(), inline) {
+        error_led.set(timestamp.get_seconds_l() & 1);
+    }
+}
+
 // Main
-X_MAIN$(cpu_freq = 8000000) {
+X_MAIN$(cpu_freq = 1000000) {
+    timestamp.reset();
+    c.start();
+    sei();
 }
